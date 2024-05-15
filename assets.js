506fe49368a2247aae51bee94b5b8eb8aa972fed
kkenrl <i@akkenrl.com>
2024-05-15 11:03:27 +0300

    upd

---
+++ assets.js
@@ -0,0 +1,59 @@
+import * as baton_turbo_bold_italic    from "./assets/baton_turbo_bold_italic.js"
+import * as baton_turbo_bold           from "./assets/baton_turbo_bold.js"
+import * as baton_turbo_book_italic    from "./assets/baton_turbo_book_italic.js"
+import * as baton_turbo_book           from "./assets/baton_turbo_book.js"
+import * as baton_turbo_heavy          from "./assets/baton_turbo_heavy.js"
+import * as baton_turbo_heavy_italic   from "./assets/baton_turbo_heavy_italic.js"
+import * as baton_turbo_medium         from "./assets/baton_turbo_medium.js"
+import * as baton_turbo_medium_italic  from "./assets/baton_turbo_medium_italic.js"
+import * as baton_turbo_regular        from "./assets/baton_turbo_regular.js"
+import * as baton_turbo_regular_italic from "./assets/baton_turbo_regular_italic.js"
+import * as sf                         from "./assets/sf.js"
+
+import * as tt_bold_italic             from "./assets/tt-chcolates/tt_bold_italic.js"
+import * as tt_bold                    from "./assets/tt-chcolates/tt_bold.js"
+import * as tt_demibold_italic         from "./assets/tt-chcolates/tt_demibold_italic.js"
+import * as tt_demibold                from "./assets/tt-chcolates/tt_demibold.js"
+import * as tt_extrabold_italic        from "./assets/tt-chcolates/tt_extrabold_italic.js"
+import * as tt_extrabold               from "./assets/tt-chcolates/tt_extrabold.js"
+import * as tt_extralight_italic       from "./assets/tt-chcolates/tt_extralight_italic.js"
+import * as tt_extralight              from "./assets/tt-chcolates/tt_extralight.js"
+import * as tt_italic                  from "./assets/tt-chcolates/tt_italic.js"
+import * as tt_light_italic            from "./assets/tt-chcolates/tt_light_italic.js"
+import * as tt_light                   from "./assets/tt-chcolates/tt_light.js"
+import * as tt_medium_light            from "./assets/tt-chcolates/tt_medium_italic.js"
+import * as tt_medium                  from "./assets/tt-chcolates/tt_medium.js"
+import * as tt                         from "./assets/tt-chcolates/tt.js"
+
+
+let assets = {}
+
+assets.batonTurboBoldItalic        = baton_turbo_bold_italic.base64;
+assets.batonTurboBold              = baton_turbo_bold.base64;
+assets.batonTurboBookItalic        = baton_turbo_book_italic.base64;
+assets.batonTurboBook              = baton_turbo_book.base64;
+assets.batonTurboHeavy             = baton_turbo_heavy.base64;
+assets.batonTurboHeavyItalic       = baton_turbo_heavy_italic.base64;
+assets.batonTurboMedium            = baton_turbo_medium.base64;
+assets.batonTurboMediumItalic      = baton_turbo_medium_italic.base64;
+assets.batonTurboRegular           = baton_turbo_regular.base64;
+assets.batonTurboRegularItalic     = baton_turbo_regular_italic.base64;
+
+assets.sf                          = sf.base64;
+
+assets.tt_bold_italic              = tt_bold_italic.base64;
+assets.tt_bold                     = tt_bold.base64;
+assets.tt_demibold_italic          = tt_demibold_italic.base64;
+assets.tt_demibold                 = tt_demibold.base64;
+assets.tt_extrabold_italic         = tt_extrabold_italic.base64;
+assets.tt_extrabold                = tt_extrabold.base64;
+assets.tt_extralight_italic        = tt_extralight_italic.base64;
+assets.tt_extralight               = tt_extralight.base64;
+assets.tt_italic                   = tt_italic.base64;
+assets.tt_light_italic             = tt_light_italic.base64;
+assets.tt_light                    = tt_light.base64;
+assets.tt_medium_light             = tt_medium_light.base64;
+assets.tt_medium                   = tt_medium.base64;
+assets.tt                          = tt.base64;
+
+export {assets}
